fd61ee0307a8db46c8de49b5474d88ae69a5ac59
joshpson <jpearson846@gmail.com>
2018-09-25 17:52:57 -0400

    explore dc text

--- src/components/activities.js
+++ src/components/activities.js
@@ -12,7 +12,12 @@ class Activities extends React.Component {
         <div className="row">
           <div className="col-12 col-md-6">
             <h1>Explore DC</h1>
-            <p className="location-description">Details to come.</p>
+            <p className="location-description">
+              We love DC, and we know you will too!<br />
+              <br /> Closer to the wedding date we'll post some of our favorite
+              museums, activities, and restaurants for you to explore during
+              your weekend in DC.{" "}
+            </p>
           </div>
         </div>
       </div>
